fa36f1967c30ec7624fea6486283b41816b3ddab
StuWares <stuwares@outlook.com>
2025-03-25 15:32:38 +0000

    Adding first unit test

---
+++ src/test_textnode.py
@@ -0,0 +1,35 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://ba2h.com")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://ba2h.com")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://ba2h.com")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://ba2h.com)", repr(node)
+        )
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
